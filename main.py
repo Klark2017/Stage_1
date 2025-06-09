a93786cc59474d5d4c561281bba42faf441f55a4
unknown <klark2017@gmail.com>
2025-06-09 20:01:00 +0300

    Add file

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Напишите функцию, которая возвращает true, если число является четным.
+Подсказка: используйте % для определения остатка от деления: 10 % 3 = 1
+"""
+
+
+def is_odd(n: int) -> bool:
+    if n = 0:
+        print("Number is zero")
+    if n % 2 == 0:
+        return True
+    return False
+
+assert is_odd(4)
+assert not is_odd(9)
+assert(0)
