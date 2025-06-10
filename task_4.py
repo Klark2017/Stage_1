e0b65c5c76c3baae557357a80e4e5895ae5260f1
unknown <klark2017@gmail.com>
2025-06-10 20:25:30 +0300

    code correction

--- task_4.py
+++ task_4.py
@@ -6,15 +6,15 @@
 
 def get_triangle_kind(a: int, b: int, c: int) -> str:
     if a == b and b == c:
-        return "равносторонний"
-        print("равносторонний")
+        triangle = "равносторонний"
+        return triangle
     elif a == b or b == c or c == a:
-        return "равнобедренный"
-        print("равнобедренный")
+        triangle = "равнобедренный"
+        return triangle
     else:
-        return "обычный"
-    print(get_triangle_kind(26, 26, 26))
+        triangle = "обычный"
+        return triangle
 
-get_triangle_kind(26, 26, 26)
-get_triangle_kind(68, 95,43)
-get_triangle_kind(45, 45, 40)
+print(get_triangle_kind(26, 26, 26))
+print(get_triangle_kind(68, 95,43))
+print(get_triangle_kind(45, 45, 40))
